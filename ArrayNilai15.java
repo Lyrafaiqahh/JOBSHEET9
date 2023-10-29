fba0f8b09eed0c2806107299584121f4dd1d2dd9
Lyrafaiqahh <lyrafaiqah@gmail.com>
2023-10-29 08:31:38 +0700

    PERCOBAAN 2 JOBSHEET 9 JAWABAN NO.4

--- ArrayNilai15.java
+++ ArrayNilai15.java
@@ -8,18 +8,20 @@ public class ArrayNilai15 {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = input.nextInt();
         }
+        System.out.println("--------------------------------------");
         for (int i = 0; i < nilaiAkhir.length; i++) {
-            if (nilaiAkhir[i] > 70) {
+            if (nilaiAkhir[i] >=70 && nilaiAkhir[i] <= 100) {
+        
                 System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }
+            else if (nilaiAkhir[i] <=69 && nilaiAkhir[i] <= 100){
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
+            else {
+                System.out.println("Maaf, nilai pada mahasiswa ke-"+i+" tidak terdeteksi, anda hanya bisa inputkan nilai 0-100, silahkan coba lagi!");
             }
             
         }
-        for (int i = 0; i < nilaiAkhir.length; i++) {
-            if (nilaiAkhir[i] < 70) {
-                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
-    
-    }
-    
-}
+        
     }
 }
